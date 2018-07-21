5bf76c2c8cf65234664c7aaf4986cb7a8b6bd256
Punyugi2012 <58160216@go.buu.ac.th>
2018-07-21 17:49:34 +0700

    เพิ่ม AppManager

--- RizzoApp/AppManager.swift
+++ RizzoApp/AppManager.swift
@@ -7,8 +7,83 @@
 //
 
 import Foundation
+import AVFoundation
 
+
+let backgroundSoundKey = "BackgroundSoundKey"
+let buttonSoundKey = "ButtonSoundKey"
 
 class AppManager {
     static let shared = AppManager()
+    
+    private let backgroundPlayer: AVAudioPlayer? = {
+        guard let url = Bundle.main.url(forResource: "bgSound", withExtension: "mp3") else {
+            return nil
+        }
+        do {
+            let player = try AVAudioPlayer(contentsOf: url)
+            
+            return player
+        }catch {
+            return nil
+        }
+    }()
+    
+    private let buttonPlayer: AVAudioPlayer? = {
+        guard let url = Bundle.main.url(forResource: "btnSound", withExtension: "mp3") else {
+            return nil
+        }
+        do {
+            let player = try AVAudioPlayer(contentsOf: url)
+            
+            return player
+        }catch {
+            return nil
+        }
+    }()
+    
+    func playBackgroundSound() {
+        AppManager.shared.backgroundPlayer?.play()
+    }
+    
+    func stopBackgroundSound() {
+        AppManager.shared.backgroundPlayer?.stop()
+    }
+    
+    func playButtonSound() {
+        if UserDefaults.standard.bool(forKey: buttonSoundKey) {
+            AppManager.shared.buttonPlayer?.stop()
+            AppManager.shared.buttonPlayer?.currentTime = 0
+            AppManager.shared.buttonPlayer?.play()
+        }
+    }
+    
+    func set(key k: String, value v: Bool) {
+        UserDefaults.standard.set(v, forKey: k)
+        UserDefaults.standard.synchronize()
+    }
+    
+    func get(key k: String) -> Bool {
+        return UserDefaults.standard.bool(forKey: k)
+    }
+    
+    func activate() {
+        if UserDefaults.standard.object(forKey: backgroundSoundKey) != nil && UserDefaults.standard.object(forKey: buttonSoundKey) != nil {
+            let background = AppManager.shared.get(key: backgroundSoundKey)
+            if background {
+                AppManager.shared.playBackgroundSound()
+            }
+            else {
+                AppManager.shared.stopBackgroundSound()
+            }
+        }
+        else {
+            print("First Launch App")
+            AppManager.shared.set(key: backgroundSoundKey, value: true)
+            AppManager.shared.set(key: buttonSoundKey, value: true)
+            AppManager.shared.playBackgroundSound()
+        }
+    }
+    
 }
+
---
+++ RizzoApp/Extensions/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  RizzoApp
+//
+//  Created by punyawee  on 21/7/61.
+//  Copyright © พ.ศ. 2561 Punyugi. All rights reserved.
+//
+
+
+import UIKit
+
+extension UIButton {
+    func playButtonSound() {
+        AppManager.shared.playButtonSound()
+    }
+}
