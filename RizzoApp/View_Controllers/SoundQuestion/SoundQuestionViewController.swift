81d803efb865cbbbd015a4de1243f40838c6a985
Punyugi2012 <58160216@go.buu.ac.th>
2018-07-21 18:04:03 +0700

    เพิ่ม viewcontroller ให้กับแต่ละ view

---
+++ RizzoApp/View_Controllers/SoundQuestion/SoundQuestionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SoundQuestionViewController.swift
+//  RizzoApp
+//
+//  Created by punyawee  on 21/7/61.
+//  Copyright © พ.ศ. 2561 Punyugi. All rights reserved.
+//
+
+import UIKit
+
+class SoundQuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
