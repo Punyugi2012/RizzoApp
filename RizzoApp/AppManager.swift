578fb3e545d1469ae17164067e5b88f43970180a
Punyugi2012 <58160216@go.buu.ac.th>
2018-07-21 16:39:00 +0700

    update

---
+++ RizzoApp/AppManager.swift
@@ -0,0 +1,14 @@
+//
+//  AppManager.swift
+//  RizzoApp
+//
+//  Created by punyawee  on 21/7/61.
+//  Copyright © พ.ศ. 2561 Punyugi. All rights reserved.
+//
+
+import Foundation
+
+
+class AppManager {
+    static let shared = AppManager()
+}
