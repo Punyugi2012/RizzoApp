46734c4dd9568620831cf63198ea66d35990f340
Punyugi2012 <58160216@go.buu.ac.th>
2018-07-22 18:41:55 +0700

    update

---
+++ RizzoApp/OtherFiles/Reusables/ModalView/ModalView.swift
@@ -0,0 +1,13 @@
+//
+//  ModalView.swift
+//  RizzoApp
+//
+//  Created by punyawee  on 22/7/61.
+//  Copyright © พ.ศ. 2561 Punyugi. All rights reserved.
+//
+
+import UIKit
+
+class ModalView: UIView {
+
+}
