534cf0e9a78059359cef11f123c4eb3c7bd9feeb
Punyugi2012 <58160216@go.buu.ac.th>
2018-07-22 14:51:17 +0700

    update

--- RizzoApp/View_Controllers/MainMenu/MainMenuViewController.swift
+++ RizzoApp/View_Controllers/MainMenu/MainMenuViewController.swift
@@ -12,7 +12,23 @@ class MainMenuViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
 
+    @IBAction func backToMainMenu(segue: UIStoryboardSegue) {
+    
+    }
+    
+    @IBAction func playImgQuestion(_ sender: UIButton) {
+        performSegue(withIdentifier: "ToPrepareImgQuestion", sender: self)
+    }
+    @IBAction func playSoundQuestion(_ sender: UIButton) {
+        performSegue(withIdentifier: "ToPrepareSoundQuestion", sender: self)
+    }
+    @IBAction func playDraw(_ sender: UIButton) {
+        performSegue(withIdentifier: "ToPrepareDraw", sender: self)
+    }
+    @IBAction func setting(_ sender: UIButton) {
+        performSegue(withIdentifier: "ToSetting", sender: self)
+    }
+ 
 }
--- RizzoApp/View_Controllers/SoundQuestion/SoundQuestionViewController.swift
+++ RizzoApp/View_Controllers/SoundQuestion/SoundQuestionViewController.swift
@@ -12,24 +12,15 @@ class SoundQuestionViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        navigationItem.hidesBackButton = true
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func finishedSoundQuestion(_ sender: UIButton) {
+        performSegue(withIdentifier: "ToFinishedSoundQuestion", sender: self)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func backtoSoundQuestion(segue: UIStoryboardSegue) {
+        
     }
-    */
-
+    
 }
