ec032728ad713c003b19a58fee82be53f8a79c2f
Punyugi2012 <58160216@go.buu.ac.th>
2018-07-23 18:54:07 +0700

    update

--- RizzoApp/View_Controllers/Draw/DrawViewController.swift
+++ RizzoApp/View_Controllers/Draw/DrawViewController.swift
@@ -13,16 +13,40 @@ class DrawViewController: UIViewController {
     var getQuestion: DrawQuestion!
     var questionType: TypeDrawQuestion!
     
+    @IBOutlet weak var blueColorBtn: UIButton!
+    @IBOutlet weak var greenColorBtn: UIButton!
+    @IBOutlet weak var yellowColorBtn: UIButton!
+    @IBOutlet weak var redColorBtn: UIButton!
+    @IBOutlet weak var blackColorBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setNavigationBar()
+        [redColorBtn, yellowColorBtn, greenColorBtn, blueColorBtn].forEach { (colorBtn) in
+            colorBtn?.transform.translatedBy(x: blackColorBtn.center.x - colorBtn!.center.x, y: 0)
+            colorBtn?.alpha = 0
+        }
         navigationItem.hidesBackButton = true
-//        print(questionType)
-//        print(getQuestion)
+    }
+    
+    func resetColorButtons() {
+        [blackColorBtn, redColorBtn, yellowColorBtn, greenColorBtn, blueColorBtn].forEach { (colorBtn) in
+           
+        }
     }
 
-  
-
+    @IBAction func tappedBlackColor(_ sender: UIButton) {
+        sender.playButtonSound()
+        var delay = 0.1
+        [redColorBtn, yellowColorBtn, greenColorBtn, blueColorBtn].forEach { (colorBtn) in
+            UIView.animate(withDuration: 0.5, delay: delay, options: [], animations: {
+                colorBtn?.transform = CGAffineTransform.identity
+                colorBtn?.alpha = 1
+            })
+            delay += 0.1
+        }
+    }
+    
 
 
 }
