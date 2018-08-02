c9d2695fa41c47551fd87e7de89b9f2d173648da
Punyugi2012 <58160216@go.buu.ac.th>
2018-08-02 13:42:31 +0700

    fixed draw scene

--- RizzoApp/View_Controllers/FinishedDraw/FinishedDrawViewController.swift
+++ RizzoApp/View_Controllers/FinishedDraw/FinishedDrawViewController.swift
@@ -14,28 +14,40 @@ class FinishedDrawViewController: UIViewController {
     var getQuestionName = ""
     var getDrawedImage: UIImage!
     
+    @IBOutlet weak var borderView: UIView!
     @IBOutlet weak var resultImageView: UIImageView!
     @IBOutlet weak var drawedImageView: UIImageView!
     @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setNavigationBar()
         print(isCorrect)
         print(getQuestionName)
+        setView()
+    }
+    
+    func setView() {
         if isCorrect {
             resultLabel.text = "มันคือ \(getQuestionName)"
             resultImageView.image = #imageLiteral(resourceName: "Finished/correct")
+            borderView.layer.borderColor = UIColor.green.cgColor
         }
         else {
             resultLabel.text = "มันยังไม่ใช่ \(getQuestionName)"
             resultImageView.image = #imageLiteral(resourceName: "Finished/incorrect")
+            borderView.layer.borderColor = UIColor.red.cgColor
         }
+        borderView.layer.cornerRadius = 10
+        borderView.clipsToBounds = true
+        borderView.layer.borderWidth = 2
         drawedImageView.image = getDrawedImage
         if !AppManager.shared.get(key: isLightThemeKey) {
             view.backgroundColor = AppManager.shared.currentTheme?.backgroundColor
         }
         resultLabel.textColor = AppManager.shared.currentTheme?.fontColor
+        titleLabel.text = "ผลการวาด\(getQuestionName)"
     }
 
     @IBAction func replay(_ sender: UIButton) {
